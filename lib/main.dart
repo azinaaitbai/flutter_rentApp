a00ea6f4b5ec102d5decdaaa5856fc68684978c5
azinaaitbai <ajtbajazina@gmail.com>
2024-04-30 13:10:40 +0600

    add

---
+++ lib/main.dart
@@ -0,0 +1,133 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    _selectedIndex = index;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: PreferredSize(
+          preferredSize: Size.fromHeight(80.0),
+          child: AppBar(
+            backgroundColor: Colors.white,
+            title: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  'Посуточная арендa',
+                  style: TextStyle(
+                    color: Colors.black87,
+                    fontSize: 30,
+                  ),
+                ),
+                Text(
+                  'недвижемости',
+                  style: TextStyle(
+                    color: Colors.black87,
+                    fontSize: 23,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        body: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Align(
+            alignment: Alignment.topLeft,
+            child: Column(
+              children: <Widget>[
+                Card(
+                  margin: EdgeInsets.all(16.0),
+                  child: Padding(
+                    padding: EdgeInsets.all(16.0),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Icon(Icons.language, color: Colors.blue.shade900),
+                            SizedBox(width: 8),
+                            Text(
+                              'Где арендовать',
+                              style: TextStyle(color: Colors.blue.shade800),
+                            ),
+                          ],
+                        ),
+                        Divider(
+                          height: 20,
+                          thickness: 1,
+                          color: Colors.grey.shade300,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Icon(Icons.access_time, color: Colors.blue.shade900),
+                            SizedBox(width: 8),
+                            Text(
+                              'Когда',
+                              style: TextStyle(color: Colors.blue.shade800),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                SizedBox(height: 16),
+                Card(
+                  margin: EdgeInsets.all(16.0),
+                  child: Padding(
+                    padding: EdgeInsets.all(16.0),
+                    child: Row(
+                      children: <Widget>[
+                        Icon(Icons.home, color: Colors.blue.shade900),
+                        SizedBox(width: 8),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text('Мои заселения', style: TextStyle(color: Colors.blue.shade900)),
+                            Text('Предстоящие и предыдущие', style: TextStyle(color: Colors.grey)),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _selectedIndex,
+          onTap: _onItemTapped,
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.vpn_key),
+              label: 'Снять',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.lock),
+              label: 'Сдать',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              label: 'Профиль',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
